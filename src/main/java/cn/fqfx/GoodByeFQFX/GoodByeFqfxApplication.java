1be0e4dde2c55095234a46a37a22d107a81556ac
ElementCraft <976228284@qq.com>
2018-01-27 02:12:44 +0800

    改用Themeleaf

--- src/main/java/cn/fqfx/GoodByeFQFX/GoodByeFqfxApplication.java
+++ src/main/java/cn/fqfx/GoodByeFQFX/GoodByeFqfxApplication.java
@@ -2,8 +2,10 @@ package cn.fqfx.GoodByeFQFX;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GoodByeFqfxApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/cn/fqfx/GoodByeFQFX/controller/DefaultController.java
@@ -0,0 +1,21 @@
+package cn.fqfx.GoodByeFQFX.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class DefaultController
+{
+    @RequestMapping("/index")
+    public String index()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/adminLogin")
+    public String adminLogin()
+    {
+        return "admin_login";
+    }
+}
