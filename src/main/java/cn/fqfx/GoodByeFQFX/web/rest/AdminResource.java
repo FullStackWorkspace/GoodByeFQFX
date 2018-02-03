4820b586267d4ed0fcfd5effbcfe75a1f417f69d
ElementCraft <976228284@qq.com>
2018-02-04 00:12:27 +0800

    新增到管理员主页的mapping

--- src/main/java/cn/fqfx/GoodByeFQFX/web/rest/AdminResource.java
+++ src/main/java/cn/fqfx/GoodByeFQFX/web/rest/AdminResource.java
@@ -7,10 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -30,8 +27,8 @@ public class AdminResource {
      * @return BaseDTO
      */
     @PostMapping("/login")
-    public ResponseEntity<BaseDTO> login(String account, String password, HttpSession session, HttpServletRequest request) {
-        BaseDTO result = adminService.login(account, password, request.getRemoteAddr(), session);
+    public ResponseEntity<BaseDTO> login(Admin admin, HttpSession session, HttpServletRequest request) {
+        BaseDTO result = adminService.login(admin.getAccount(), admin.getPassword(), request.getRemoteAddr(), session);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
