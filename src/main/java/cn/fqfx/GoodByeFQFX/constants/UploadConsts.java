6fee6bd58a27bcf48e804827c4f05e53a774f850
Company Hc <1214100483@qq.com>
2018-02-07 18:20:22 +0800

    1.文件上传常用枚举

---
+++ src/main/java/cn/fqfx/GoodByeFQFX/constants/UploadConsts.java
@@ -0,0 +1,20 @@
+package cn.fqfx.GoodByeFQFX.constants;
+
+/**
+ * 文件上传常量类
+ */
+public final class UploadConsts {
+    /**
+     * 删除文件文件为空
+     */
+    public final static Integer ERROR_FILE_DELETE_EMPTY_PARAM = -1;
+    /**
+     * 文件长传成功
+     */
+    public final static Integer SUCCESS_FILE_ADD_PARAM = 0;
+    /**
+     * 上传文件时文件为空
+     */
+    public final static Integer ERROR_FILE_ADD_EMPTY_PARAM = 1;
+
+}
